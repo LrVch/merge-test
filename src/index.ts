fc78520d3c15b640ebe6c4f45e65a16261cdd576
RLonskiy <RLonskiy@luxoft.com>
2019-01-19 16:16:06 +0600

    done

--- src/index.ts
+++ src/index.ts
@@ -1,173 +1,81 @@
-import * as PureJs from './es5-extra/index';
-
-export type LogObj = (obj: Object, desc?: string) => void;
-
-export type Line = (logn?: number) => void;
-
-export const line: Line = (logn: number = 20) => console.log('='.repeat(logn));
-
-export const logObj: LogObj = (obj: Object = {}, desc: string = '') => {
-    console.log(desc, JSON.stringify(obj, null, 4));
+function noDuplicate(arr: number[]): number[] {
+    const s = new Set(arr);
+    const it = s.values();
+    return Array.from(it);
 }
-
-export interface ICook {
-    cook: boolean;
-}
-
-export interface IDancer {
-    dance: boolean;
-}
-
-export interface ITeacher {
-    teach: boolean;
-}
-
-export interface ISinger {
-    sing: boolean;
-}
-
-export interface ICoder {
-    code?: boolean;
-}
-
-export interface IPilot {
-    flyPlanes?: boolean;
-}
-
-export interface IFootballPlayer {
-    playFootball?: boolean;
-}
-
-export interface IShooter {
-    shootGuns?: boolean;
-}
-
-export interface IGeek {
-    repairElectronics?: boolean;
-}
-
-export interface IGeneralAbilities extends ICook, IDancer, ITeacher, ISinger { }
-
-export interface IHobby extends ICoder, IPilot, IFootballPlayer, IShooter, IGeek { }
-
-export interface IHuman extends IGeneralAbilities { }
-
-export abstract class AbstarctHuman implements IHuman {
-    cook = false;
-    sing = false;
-    dance = false;
-    teach = false;
-}
-
-export class Human extends AbstarctHuman {
-    constructor(options: Partial<IHuman> = {}) {
-        super();
-        return Object.assign(this, options);
-    }
-}
-
-type JakeOptions = Partial<IHuman> & Pick<IHobby, 'flyPlanes' | 'playFootball'>;
-
-export class Jake extends Human implements IHobby {
-    playFootball = false;
-    flyPlanes = false;
-
-    constructor(options: JakeOptions = {}) {
-        super({ ...options, dance: true });
-        return Object.assign(this, options);
-    }
-}
-
-type JaneOptions = Partial<IHuman> & Pick<IHobby, 'code'>;
-
-export class Jane extends Human implements IHobby {
-    code = false;
-
-    constructor(options: JaneOptions = {}) {
-        super({ ...options, teach: true });
-        return Object.assign(this, options);
-    }
-}
-
-type JanisOptions = Partial<IHuman> & Pick<IHobby, 'shootGuns' | 'repairElectronics'>;
-
-export class Janis extends Human implements IHobby {
-    shootGuns = false;
-    repairElectronics = false;
-
-    constructor(options: JanisOptions = {}) {
-        super({ ...options, cook: true });
-        return Object.assign(this, options);
-    }
-}
-
-const human1 = new Human();
-const human2 = new Human({ sing: true, dance: true });
-
-const jake1 = new Jake({ sing: true });
-const jake2 = new Jake({ playFootball: true, flyPlanes: true });
-
-const jane1 = new Jane();
-const jane2 = new Jane({ code: true });
-
-const janis1 = new Janis();
-const janis2 = new Janis({ shootGuns: true, repairElectronics: true });
-
-logObj(human1, 'human1');
-logObj(human2, 'human2');
-
-line();
-
-logObj(jake1, 'jake1');
-logObj(jake2, 'jake2');
-
-line();
-
-logObj(jane1, 'jane1');
-logObj(jane2, 'jane2');
-
-line();
-
-logObj(janis1, 'janis1');
-logObj(janis2, 'janis2');
-
 
 /* 
-    Extra es5
+    merge as itarator
 */
 
+class Merge implements Iterable<number[]> {
+    constructor(public arr: number[][]) { }
 
-line();
-console.log('Extra es5');
-line();
+    [Symbol.iterator]() {
+        const source = this.arr.slice();
+        let index = 0;
+        let result: number[] = [];
 
-const humanPure1 = new PureJs.Human();
-const humanPure2 = new PureJs.Human({ sing: true, dance: true });
+        return {
+            next(): IteratorResult<number[]> {
+                if (index < source.length) {
+                    result = [...result, ...source[index]];
+                    result = noDuplicate(result);
+                    index++;
+                    return {
+                        done: false,
+                        value: result.sort()
+                    };
+                } else {
+                    return {
+                        done: true,
+                        value: null
+                    };
+                }
+            }
+        }
+    }
+}
 
-const jakePure1 = new PureJs.Jake({ sing: true });
-const jakePure2 = new PureJs.Jake({ playFootball: true, flyPlanes: true });
+function merge(arr: number[][]): number[] {
+    const result = [...new Merge(arr)];
 
-const janePure1 = new PureJs.Jane();
-const janePure2 = new PureJs.Jane({ code: true });
+    return result[result.length - 1];
+}
 
-const janisPure1 = new PureJs.Janis();
-const janisPUre2 = new PureJs.Janis({ shootGuns: true, repairElectronics: true });
+console.log(merge([[1, 1, 2, 2, 3, 4], [2, 3, 4, 5, 5]]));
 
+/* 
+    merge as generator
+*/
 
-logObj(humanPure1, 'humanPure1');
-logObj(humanPure2, 'humanPure2');
+export interface IGenerator {
+    next: (value: any) => any;
+}
 
-line();
+function* mergeGen(arr: number[][]) {
+    let result: number[] = [];
+    for (let a of arr) {
+        const arr = yield a;
+        result = [...result, ...arr];
+        result = noDuplicate(result);
+    }
+    return result;
+}
 
-logObj(jakePure1, 'jakePure1');
-logObj(jakePure2, 'jakePure2');
+async function mergeArr(arr: number[][]) {
+    return await new Promise(res => {
+        function execute(generator: IGenerator, yieldValue?: any) {
+            let next = generator.next(yieldValue);
 
-line();
+            if (!next.done) {
+                execute(generator, next.value);
+            } else {
+                res(next.value);
+            }
+        }
+        execute(mergeGen(arr));
+    });
+}
 
-logObj(janePure1, 'janePure1');
-logObj(janePure2, 'janePure2');
-
-line();
-
-logObj(janisPure1, 'janisPure1');
-logObj(janisPUre2, 'janisPUre2');
+mergeArr([[1, 1, 2, 2, 3, 4], [2, 3, 4, 5, 5]]).then(console.log);
